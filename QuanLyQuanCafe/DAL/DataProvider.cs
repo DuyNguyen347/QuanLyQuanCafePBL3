15de90b1277069942090a5af847c4cc3ab38a1d8
= <=>
2022-05-03 13:13:43 +0700

    3/5

--- QuanLyQuanCafe/DAL/DataProvider.cs
+++ QuanLyQuanCafe/DAL/DataProvider.cs
@@ -36,6 +36,7 @@ namespace QuanLyQuanCafe.DAL
             s = ConfigurationManager.ConnectionStrings["QuanLyQuanCafeConnectionString"].ConnectionString;
             //s = @"Data Source=DESKTOP-KMNS09Q\SQLEXPRESS;Initial Catalog=QL_cafe;Integrated Security=True";
             //connect to tĩnh network
+            // Đà Nẵng
             //s = @"Data Source = 116.105.164.50,1433; Initial Catalog = QL_cafe; User ID = NQT; Password = 68709502";
             //s = @"Data Source = 14.165.149.140,1433; Initial Catalog = QL_cafe; User ID = NQT; Password = 68709502";
         }
